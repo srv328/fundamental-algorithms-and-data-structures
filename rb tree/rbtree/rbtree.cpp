0d596eb301314cc69678ad3c974b1f6ed8ca8c09
shevelev roman <75514740+srv328@users.noreply.github.com>
2023-12-09 13:49:03 +1000

    Update rbtree.cpp

--- rb tree/rbtree/rbtree.cpp
+++ rb tree/rbtree/rbtree.cpp
@@ -1,80 +1,26 @@
-﻿#include <iostream>
+#pragma once
+#include <iostream>
 #include <Windows.h>
+#include "List.h"
+#include <sstream>
 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-template <typename KEY>;
-struct list_elem {
-	KEY key;
-	int index;
-	list_elem* next, * prev;
-	int count;
-};
-
-list_elem* list_init()
-{
-	list_elem* root = nullptr;
-	return root;
-}
-
-template <typename KEY>;
-void list_insert(list_elem*& head, KEY data)
-{
-	list_elem* insert = new(list_elem);
-	insert->count = 1;
-	insert->data = data;
-	insert->next = insert;
-	insert->prev = insert;
-	if (head == nullptr) head = insert;
-	else head->count++;
-}
-
-
-template <typename KEY>;
-void list_erase(list_elem*& head, KEY data) {
-	if (head != nullptr) {
-		if (head->count == 1) {
-			head->count--;
-			delete head;
-			head = nullptr;
-		}
-		else head->count--;
-	}
-}
-
-void set_color(char a) {
-	if (a == 'R') SetConsoleTextAttribute(hConsole, 12);
-	else SetConsoleTextAttribute(hConsole, 15);
-}
-
-void print_list(list_elem* root, bool color)
-{
-	if (root != nullptr) {
-		if (color) set_color('B');
-		else set_color('R');
-		list_elem* temp = root;
-		while (temp->next != root) {
-			std::cout << temp->data.sym << temp->data.num << " (" << temp->count << ")";
-			temp = temp->next;
-		}
-		std::cout << temp->data.sym << temp->data.num << " (" << temp->count << ")";
-	}
-	SetConsoleTextAttribute(hConsole, 15);
-}
-
-struct tree_elem {
+template <typename T>
+struct tree_elem_generic {
+	T data;
 	list_elem* list;
-	tree_elem* left, * right, * parent;
+	tree_elem_generic* left, * right, * parent;
 	bool color; // True - Black
 };
 
-tree_elem* root_init(tree_elem* nullnode) {
-	tree_elem* root = nullnode;
+template <typename T>
+tree_elem_generic<T>* root_init_generic(tree_elem_generic<T>* nullnode) {
+	tree_elem_generic<T>* root = nullnode;
 	return root;
 }
 
-tree_elem* null_init() {
-	tree_elem* nullnode = new tree_elem;
+template <typename T>
+tree_elem_generic<T>* null_init_generic() {
+	tree_elem_generic<T>* nullnode = new tree_elem_generic<T>;
 	nullnode->left = nullptr;
 	nullnode->right = nullptr;
 	nullnode->list = nullptr;
@@ -82,30 +28,20 @@ tree_elem* null_init() {
 	return nullnode;
 }
 
-tree_elem* nullnode = null_init();
-
-template <typename KEY>;
-int compare(KEY data1, KEY data2) {
-	if (data1 > data2) return 1;
-	if (data1 < data2) return -1;
-	return 0;
-}
-
-
-template <typename KEY>;
-bool search(tree_elem* root, tree_elem* nullnode, KEY data) {
-	tree_elem* temp = root;
+template <typename T>
+tree_elem_generic<T>* search(tree_elem_generic<T>* root, tree_elem_generic<T>* nullnode, T data) {
+	tree_elem_generic<T>* temp = root;
 	while (temp != nullnode) {
-		if (compare(temp->list->data, data) == 0) return true;
-		if (compare(data, temp->list->data) == 1) temp = temp->right;
+		if (temp->data == data) return temp;
+		if (data > temp->data) temp = temp->right;
 		else temp = temp->left;
 	}
-	return false;
+	return nullnode;
 }
 
-
-void rightRotate(tree_elem* element, tree_elem*& root, tree_elem* nullnode) {
-	tree_elem* y = element->left;
+template <typename T>
+void rightRotate(tree_elem_generic<T>* element, tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode) {
+	tree_elem_generic<T>* y = element->left;
 	element->left = y->right;
 	if (y->right != nullnode) y->right->parent = element;
 	y->parent = element->parent;
@@ -116,8 +52,9 @@ void rightRotate(tree_elem* element, tree_elem*& root, tree_elem* nullnode) {
 	element->parent = y;
 }
 
-void leftRotate(tree_elem* element, tree_elem*& root, tree_elem* nullnode) {
-	tree_elem* y = element->right;
+template <typename T>
+void leftRotate(tree_elem_generic<T>* element, tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode) {
+	tree_elem_generic<T>* y = element->right;
 	element->right = y->left;
 	if (y->left != nullnode) y->left->parent = element;
 	y->parent = element->parent;
@@ -128,8 +65,9 @@ void leftRotate(tree_elem* element, tree_elem*& root, tree_elem* nullnode) {
 	element->parent = y;
 }
 
-void insert_balance(tree_elem*& element, tree_elem*& root, tree_elem* nullnode) {
-	tree_elem* uncle;
+template <typename T>
+void insert_balance(tree_elem_generic<T>*& element, tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode) {
+	tree_elem_generic<T>* uncle;
 	while (element->parent->color == false) {
 		if (element->parent == element->parent->parent->left) {
 			uncle = element->parent->parent->right;
@@ -174,29 +112,37 @@ void insert_balance(tree_elem*& element, tree_elem*& root, tree_elem* nullnode) 
 
 }
 
+template <typename T>
+tree_elem_generic<T>* insert(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode, T data, int index) {
+	T key = data;
+	tree_elem_generic<T>* existingElement = search(root, nullnode, key);
 
-template <typename KEY>;
-tree_elem* insert(tree_elem*& root, tree_elem* nullnode, KEY data) {
-	KEY key = data;
-	if (!search(root, nullnode, key)) {
-		tree_elem* element = new tree_elem;
+	if (existingElement == nullnode) {
+		// Ёлемент с данным ключом не существует, создаем новый
+		tree_elem_generic<T>* element = new tree_elem_generic<T>;
 		element->parent = nullptr;
 		element->left = nullnode;
 		element->right = nullnode;
 		element->color = false;
 		element->list = list_init();
-		list_insert(element->list, key);
-		tree_elem* parent_buffer = nullptr;
-		tree_elem* runner = root;
+		list_insert(element->list, index);
+		element->data = key;
+
+		tree_elem_generic<T>* parent_buffer = nullptr;
+		tree_elem_generic<T>* runner = root;
+
 		while (runner != nullnode) {
 			parent_buffer = runner;
-			if (compare(element->list->data, runner->list->data) == -1) runner = runner->left;
+			if (element->data < runner->data) runner = runner->left;
 			else runner = runner->right;
 		}
+
 		element->parent = parent_buffer;
+
 		if (parent_buffer == nullptr) root = element;
-		else if (compare(element->list->data, parent_buffer->list->data) == -1) parent_buffer->left = element;
+		else if (element->data < parent_buffer->data) parent_buffer->left = element;
 		else { parent_buffer->right = element; }
+
 		if (element->parent == nullptr) {
 			element->color = true;
 			return root;
@@ -205,26 +151,22 @@ tree_elem* insert(tree_elem*& root, tree_elem* nullnode, KEY data) {
 		insert_balance(element, root, nullnode);
 	}
 	else {
-		tree_elem* temp = root;
-		while (temp != nullnode) {
-			if (compare(temp->list->data, key) == 0) { list_insert(temp->list, key); break; }
-			if (compare(key, temp->list->data) == 1) { temp = temp->right; }
-			else { temp = temp->left; }
-		}
-		return root;
+		list_insert(existingElement->list, index);
 	}
-	return nullnode;
+	return root;
 }
 
-void replace_node(tree_elem*& root, tree_elem* nullnode, tree_elem* n, tree_elem* child) {
+template <typename T>
+void replace_node(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode, tree_elem_generic<T>* n, tree_elem_generic<T>* child) {
 	if ((n->parent == nullnode) || (n->parent == nullptr)) root = child;
 	else if (n == n->parent->left) n->parent->left = child;
 	else n->parent->right = child;
 	child->parent = n->parent;
 }
 
-void erase_balance(tree_elem*& root, tree_elem* nullnode, tree_elem* element) {
-	tree_elem* brother;
+template <typename T>
+void erase_balance(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode, tree_elem_generic<T>* element) {
+	tree_elem_generic<T>* brother;
 	while ((element != root) && (element->color == 1)) {
 		if (element == element->parent->left) {
 			brother = element->parent->right;
@@ -282,27 +224,43 @@ void erase_balance(tree_elem*& root, tree_elem* nullnode, tree_elem* element) {
 	element->color = true;
 }
 
-tree_elem* left_max(tree_elem* element, tree_elem* nullnode) {
+template <typename T>
+tree_elem_generic<T>* left_max(tree_elem_generic<T>* element, tree_elem_generic<T>* nullnode) {
 	while (element->right != nullnode) element = element->right;
 	return element;
 }
 
+template <typename T>
+void search_between(tree_elem_generic<T>* root, tree_elem_generic<T>* nullnode, int year1, int year2, list_elem*& result_list) {
+	if (root == nullnode) {
+		return;
+	}
+	search_between(root->left, nullnode, year1, year2, result_list);
+	if (year1 <= root->data && root->data <= year2) {
+		list_elem* temp = root->list;
+		do {
+			list_insert(result_list, temp->key);
+			temp = temp->next;
+		} while (temp != root->list);
+	}
+	search_between(root->right, nullnode, year1, year2, result_list);
+}
 
-template <typename KEY>;
-tree_elem* erase(tree_elem*& root, tree_elem* nullnode, KEY data) {
-	if (root != nullptr) {
-		KEY key = data;
-		tree_elem* temp = root;
-		tree_elem* z = nullnode;
-		tree_elem* element;
-		tree_elem* y;
+template <typename T>
+tree_elem_generic<T>* erase(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode, T data, int number) {
+	if (root != nullnode && root != nullptr) {
+		T key = data;
+		tree_elem_generic<T>* temp = root;
+		tree_elem_generic<T>* z = nullnode;
+		tree_elem_generic<T>* element;
+		tree_elem_generic<T>* y;
 		while (temp != nullnode) {
-			if (compare(temp->list->data, key) == 0) { z = temp; }
-			if ((compare(temp->list->data, key) == 0) || (compare(temp->list->data, key) == -1)) { temp = temp->right; }
+			if (temp->data == key) { z = temp; }
+			if ((temp->data == key) || (temp->data < key)) { temp = temp->right; }
 			else { temp = temp->left; }
 		}
 		if (z == nullnode) return root;
-		if (z->list->count > 1) list_erase(z->list, key);
+		if (z->list->next != z->list) remove(z->list, number);
 		else {
 			y = z;
 			bool y_original_color = y->color;
@@ -331,7 +289,7 @@ tree_elem* erase(tree_elem*& root, tree_elem* nullnode, KEY data) {
 				y->right->parent = y;
 				y->color = z->color;
 			}
-			list_erase(z->list, key);
+			remove(z->list, number);
 			delete z;
 			if (y_original_color == true) erase_balance(root, nullnode, element);
 		}
@@ -340,13 +298,12 @@ tree_elem* erase(tree_elem*& root, tree_elem* nullnode, KEY data) {
 	return nullptr;
 }
 
-
-template <typename KEY>;
-void delete_list(tree_elem*& root, tree_elem* nullnode, KEY key) {
-	tree_elem* temp = root;
-	if (temp != nullnode) {
-		while (temp->list->count > 1) list_erase(temp->list, key);
-		list_erase(temp->list, key);
+template <typename T>
+void delete_list(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode) {
+	tree_elem_generic<T>* temp = root;
+	if (temp != nullnode && temp != nullptr) {
+		while (temp->list->next != temp->list) list_erase(temp->list);
+		list_erase(temp->list);
 		temp->left = nullptr;
 		temp->right = nullptr;
 		temp->parent = nullptr;
@@ -356,33 +313,58 @@ void delete_list(tree_elem*& root, tree_elem* nullnode, KEY key) {
 	}
 }
 
-void delete_node(tree_elem*& root, tree_elem* nullnode) {
+template <typename T>
+void delete_node(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode) {
 	if (root->left != nullnode) delete_node(root->left, nullnode);
 	if (root->right != nullnode) delete_node(root->right, nullnode);
-	delete_list(root, nullnode, root->list->data);
+	delete_list(root, nullnode);
 }
 
-tree_elem* delete_tree(tree_elem*& root, tree_elem* nullnode)
+template <typename T>
+tree_elem_generic<T>* delete_tree(tree_elem_generic<T>*& root, tree_elem_generic<T>* nullnode)
 {
 	delete_node(root, nullnode);
 	root = nullptr;
 	return nullnode;
 }
 
-void print(tree_elem* root, tree_elem* nullnode, int h, int ln) {
-	if (root != nullptr) {
-		if ((root != nullnode) && (root != nullptr)) {
-			print(root->right, nullnode, h + 4, ln);
-			for (int i = 1; i <= h; i++) std::cout << ' ';
-			print_list(root->list, root->color);
-			for (int i = 0; i < ln; i++) std::cout << '\n';
-			print(root->left, nullnode, h + 4, ln);
+template <typename T>
+void index_update(tree_elem_generic<T>* root, tree_elem_generic<T>* nullnode, int index, int cur_size) {
+	if ((root != nullnode) && (root != nullptr)) {
+		index_update(root->right, nullnode, index, cur_size);
+		if (root->list != nullptr) {
+			if (root->list->prev->key == cur_size) {
+				root->list->prev->key = index;
+			}
 		}
+		index_update(root->left, nullnode, index, cur_size);
 	}
 }
 
-int main() {
-	setlocale(LC_ALL, "Russian");
-	tree_elem* root = root_init(nullnode);
-	
+template <typename T>
+std::string print(tree_elem_generic<T>* root, tree_elem_generic<T>* nullnode, int h, int ln) {
+	if (root != nullnode) {
+		std::string result = "";
+
+		if (root->left != nullnode || root->right != nullnode || root->list != nullptr) {
+			result += print(root->right, nullnode, h + 4, ln);
+
+			for (int i = 1; i <= h; i++)
+				result += ' ';
+
+			std::stringstream ss;
+			ss << root->data;
+			result += ss.str() + print_list(root->list, root->color);
+
+			for (int i = 0; i < ln; i++)
+				result += '\n';
+
+			result += print(root->left, nullnode, h + 4, ln);
+		}
+
+		return result;
+	}
+	else {
+		return "";
+	}
 }
